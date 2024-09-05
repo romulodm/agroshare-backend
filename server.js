dd523ae99f8e4100ed1701f62342111c4fea696b
Romulo <romulotg12@gmail.com>
2024-09-05 00:29:30 -0300

    feat: calling the scraper job and config the route on server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const cron = require('node-cron');
 const path = require("path");
 const db = require('./config/db.js');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server running!');
+});
+
+require('./infra/jobs/scraperJob.js');
 
 app.use((req, res, next) => {
     if (req.headers.user) {
@@ -20,7 +25,12 @@ app.use(
     express.static(path.resolve(__dirname, ".", "tmp"))
 );
 
-db.sync().then(() => { //{force:true}
-    server.listen(process.env.PORT)
+
+const apiPrefix = '/agroshare/api/v1'
+
+app.use(apiPrefix + '/users', require('./infra/server/routes/user.js'));
+
+db.sync().then(() => { 
+    app.listen(process.env.PORT)
     console.log(`Server running on PORT: ${process.env.PORT}`);
 }).catch(err => console.log(err))
