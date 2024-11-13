9681b69e1339d6afbdc7b4bf8a1be7aaf0d90706
Romulo <romulotg12@gmail.com>
2024-11-12 22:42:36 -0300

    feat: index.js that performs the instantiation and initial configuration of the server before making it listen on a port

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+const RedisStore = require('connect-redis').default;
+const path = require("path");
+const pg = require('./config/sequelize')
+const redisClient = require('./config/redis')
+
+pg.sync()
+        .then(() => { console.log("Connected on PostgreSQL!") })
+        .catch(err => console.log('An error ocurred on PostgreSQL connection attempt:', err));
+
+const app = express();
+app.disable("x-powered-by");
+app.set("trust proxy", 1);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.REDIS_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 * 24 }
+}));
+
+app.get('/ping', (req, res) => {
+    res.send('Pong!');
+});
+
+require('./jobs/scraper.js');
+
+app.use((req, res, next) => {
+    if (req.headers.user) {
+        req.user = JSON.parse(req.headers.user); 
+    }
+    next();
+});
+
+app.use(
+    "/user-image",
+    express.static(path.resolve(__dirname, ".", "tmp"))
+);
+
+const apiPrefix = '/agroshare/api/v1'
+
+app.use(apiPrefix + '/auth', require('./server/routes/auth.route.js'));
+app.use(apiPrefix + '/users', require('./server/routes/user.route.js'));
+app.use(apiPrefix + '/email', require('./server/routes/email.route.js'));
+
+app.listen(process.env.PORT, () => {
+    console.log(`🚀 Server running on PORT: ${process.env.PORT}`);
+});
